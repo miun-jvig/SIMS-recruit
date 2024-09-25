bbcf64434ac3f390cc75d0fe4b8ad6bbfde2a23f
Måns Bodin <mabo2204@student.miun.se>
2024-09-25 14:08:44 +0200

    -setup.py added for possibility to install repos as package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="recruit_repo",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies in future
+    ],
+)
