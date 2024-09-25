0b83751361664329f49959219df765910787b803
Måns Bodin <mabo2204@student.miun.se>
2024-09-25 14:52:32 +0200

    -setup.py added for possibility to install repos as package -added explicit need of packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="recruit_repo",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(include=['agent', 'api', 'config', 'graphs', 'processing']),
     install_requires=[
         # Add dependencies in future
     ],
