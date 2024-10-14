b697111a2435ba016b9bb68b464133c29cc07d3c
miun-jvig <jovi1802@student.miun.se>
2024-10-14 18:06:44 +0200

    Removed a redundant code-snippet

--- ui/applicants.py
+++ ui/applicants.py
@@ -22,8 +22,7 @@ upload_profile_col, upload_cv_col = st.columns([1, 1])
 with upload_profile_col:
     uploaded_profile = st.file_uploader("Upload Requirement Profile", type=["txt"])
 
-    if uploaded_profile is not None and (st.session_state["requirement_profile"] is None or
-                                         st.session_state["requirement_profile"] != uploaded_profile):
+    if uploaded_profile is not None and st.session_state["requirement_profile"] != uploaded_profile:
         st.session_state["requirement_profile"] = uploaded_profile
         # Send the uploaded file to the backend to store it in the database
         with st.spinner("Uploading profile..."):
@@ -42,7 +41,7 @@ with upload_profile_col:
 with upload_cv_col:
     uploaded_cv = st.file_uploader("Upload CV", type=['txt'])
 
-    if uploaded_cv is not None and (st.session_state["cv"] is None or st.session_state["cv"] != uploaded_cv):
+    if uploaded_cv is not None and st.session_state["cv"] != uploaded_cv:
         st.session_state["cv"] = uploaded_cv
         # Check if entry_id exist before upload of cv
         if 'entry_id' not in st.session_state:
